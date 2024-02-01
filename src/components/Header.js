38021b21d495a195aeeb62ecbc51124f6d69f4bd
Eunike Pardede <eunikehedriani@gmail.com>
2024-02-01 15:25:36 +0100

    add Navlinks on Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,45 @@
-import {Navbar, NavbarBrand} from 'reactstrap';
+// import {Navbar, NavbarBrand} from 'reactstrap';
 import NucampLogo from '../app/assets/img/logo.png';
+import { useState } from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <Navbar dark color='primary' sticky='top' expand='md'>
-      <NavbarBrand href='/'>
-        <img src={NucampLogo} alt='nucamp logo' />
+      <NavbarBrand className='ms-5' href='/' > 
+        <img className='float-start' src={NucampLogo} alt='nucamp logo' />
+        <h1 className='mt-1'>NuCamp</h1>
       </NavbarBrand>
+      <Nav className='ms-auto' navbar>
+        <NavItem>
+          <NavLink className='nav-link' to='/'>
+            <i className='fa fa-home fa-lg' /> Home
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className='nav-link' to='/directory'>
+            <i className='fa fa-list fa-lg' /> Directory
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className='nav-link' to='/about'>
+            <i className='fa fa-info fa-lg' /> About
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className='nav-link' to='/contact'>
+            <i className='fa fa-address-card fa-lg' /> Contact
+          </NavLink>
+        </NavItem>
+      </Nav>
     </Navbar>
   )
 }
