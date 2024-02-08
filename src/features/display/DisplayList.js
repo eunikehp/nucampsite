c6e75fba2474319b99238c03c37a27a6aab513d8
Eunike Pardede <eunikehedriani@gmail.com>
2024-02-08 10:35:06 +0100

    implement conditional rendering for featured item in Display List

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -13,10 +13,12 @@ const DisplayList = () => {
     <Row>
       {items.map((item,idx) => {
         return (
-          <Col md className='m-1' key={idx}>
+          item && ( //implement conditional rendering
+            <Col md className='m-1' key={idx}>
             <AnimatedDisplayCard item={item} />
           </Col>
-        )
+          )
+        );
       })}
     </Row>
   )
