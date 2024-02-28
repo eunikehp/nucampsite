6307e256906ac3f6d9abc0ace9891199795b5471
Eunike Pardede <eunikehedriani@gmail.com>
2024-02-28 19:41:17 +0100

    Async Campsites

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,15 +1,48 @@
 //judul campsitesSlice, c huruf kecil menandakan bahwa the file doesn't export a react component
-import { createSlice } from '@reduxjs/toolkit';
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import { CAMPSITES } from '../../app/shared/oldData/CAMPSITES';
+import { baseUrl } from '../../app/shared/baseUrl';
+import { mapImageUrl } from '../../utils/mapImageURL';
+
+
+export const fetchCampsites = createAsyncThunk(
+  'campsites/fetchCampsites',
+  async () => {
+    const response = await fetch(baseUrl + 'campsites');
+    if(!response.ok) {
+      return Promise.reject('Unable to fetch, status:' + response.status)
+    }
+    const data = await response.json()
+    return data;
+  }
+);
+
 
 // redux :createslice & reducer
 const initialState = {
-  campsitesArray: CAMPSITES
+  campsitesArray: [],
+  isLoading: true,
+  errMsg: ''
 };
 
 const campsitesSlice = createSlice({
   name: 'campsites',
-  initialState
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchCampsites.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchCampsites.fulfilled]: (state,action) => {
+      state.isLoading = false;
+      state.errMsg = '';
+      state.campsitesArray = mapImageUrl(action.payload);
+    },
+    [fetchCampsites.rejected]: (state,action) => {
+      state.isLoading = false;
+      state.errMsg= action.error ? action.error.message : 'Fetch failed';
+    }
+  }
 });
 
 export const campsitesReducer = campsitesSlice.reducer;
@@ -20,7 +53,7 @@ export const selectAllCampsites = (state) => {
 };
 
 export const selectCampsiteById = (id) => (state) => {
-  return CAMPSITES.find(
+  return state.campsites.campsitesArray.find(
     (campsite) => campsite.id === parseInt(id)
     ); //bikin supaya bisa klik gambar dan munculin gambar detail.
 };
@@ -28,6 +61,9 @@ export const selectCampsiteById = (id) => (state) => {
 export const selectFeaturedCampsite = (state) => {
   return state.campsites.campsitesArray.find((campsite) => campsite.featured); // if featured = true, it will show.
 };
+
+
+
 
 
 // export const selectAllCampsites = () => {
