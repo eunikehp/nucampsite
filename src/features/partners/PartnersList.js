ab5f5d9c1197e1b8e35fa4ed37beca02823d09f6
Eunike Pardede <eunikehedriani@gmail.com>
2024-02-29 10:51:13 +0100

    Async Promotions

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -29,17 +29,17 @@ const PartnersList = () => {
           </Row>
       </Col>
   );
-  return (
-    <Col className='mt-4'>
-      {partners.map((partner) => {
-        return (
-          <div className= 'd-flex mb-5' key={partner.id}>
-            <Partner partner={partner} />
-          </div>
-        );
-      })}
-    </Col>
-  );
+  // return (
+  //   <Col className='mt-4'>
+  //     {partners.map((partner) => {
+  //       return (
+  //         <div className= 'd-flex mb-5' key={partner.id}>
+  //           <Partner partner={partner} />
+  //         </div>
+  //       );
+  //     })}
+  //   </Col>
+  // );
 };
 
 export default PartnersList;
