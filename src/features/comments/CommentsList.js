c9355ccb64e8eca4a4cfc89b3e91d75ceb6cd09d
Eunike Pardede <eunikehedriani@gmail.com>
2024-02-29 14:10:46 +0100

    Async Comments and update commentslist to handle error and loading state

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,14 +1,32 @@
 import { useSelector } from 'react-redux';
-import { Col } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import Comment from './Comment';
 import { selectCommentsByCampsiteId } from './commentsSlice';
 import CommentForm from './CommentForm.js';
-
+import Loading from '../../components/Loading.js';
+import Error from '../../components/Error.js';
 
 //useSelector
 const CommentsList = ({ campsiteId }) => {
   const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
   
+  const isLoading = useSelector((state) => state.comments.isLoading);
+  const errMsg = useSelector((state) => state.comments.errMsg);
+  if (isLoading) {
+    return (
+        <Row>
+            <Loading />
+        </Row>
+      );
+  }
+
+  if (errMsg) {
+      return (
+          <Row>
+              <Error errMsg={errMsg} />
+          </Row>
+      );
+  };
   // make sure that comments is not undefined or null and check if the value comments.length is greater than zero
   if (comments && comments.length > 0) {
     return (
